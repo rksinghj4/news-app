97c64af020608954a526fdce675bb6eef207d44d
Raj Kumar Singh <rksinghj4@gmail.com>
2025-03-04 10:25:55 +0530

    Add initial setup

---
+++ app/src/main/java/com/raj/newsapp/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.raj.newsapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.raj.newsapp.ui.theme.NewsAppComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            NewsAppComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MainScreen(
+                        msg = "Welcome to news app",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MainScreen(msg: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello, $msg!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NewsAppComposeTheme {
+        MainScreen("Android")
+    }
+}
